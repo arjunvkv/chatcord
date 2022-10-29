f6a82d205b6c599496c07c675db8a846e8f40b89
ifthikarudheen <ifthikarudheen.t@neoito.com>
2022-10-29 22:02:33 +0530

    completed ui

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoLogoSnapchat } from "react-icons/io";
 import { FaUsers } from "react-icons/fa";
 import { MdOutlineMeetingRoom } from "react-icons/md";
@@ -6,6 +6,47 @@ const rooms = ["javascript", "react", "sass", "node", "django", "python"];
 const users = ["steeven", "arjun", "deepak", "vyshak", "vinayak", "julian"];
 
 const Chat = () => {
+  const [messages, setMessages] = useState([
+    { message: "hai you have been disQualified", time: "1:5pm", name: "arjun" },
+    // { message: "hai you have been disQualified", time: "1:5pm", name: "jojo" },
+    // { message: "hai you have been disQualified", time: "1:5pm", name: "do" },
+    // { message: "hai you have been disQualified", time: "1:5pm", name: "not" },
+    // { message: "hai you have been disQualified", time: "1:5pm", name: "try" },
+    // { message: "hai you have been disQualified", time: "1:5pm", name: "this" },
+    // { message: "hai you have been disQualified", time: "1:5pm", name: "at" },
+    // { message: "hai you have been disQualified", time: "1:5pm", name: "home" },
+    // {
+    //   message: "hai you have been disQualified",
+    //   time: "1:5pm",
+    //   name: "adventures",
+    // },
+    // {
+    //   message: "hai you have been disQualified",
+    //   time: "1:5pm",
+    //   name: "bizare",
+    // },
+    // {
+    //   message: "hai you have been disQualified",
+    //   time: "1:5pm",
+    //   name: "chrison",
+    // },
+    // {
+    //   message: "hai you have been disQualified",
+    //   time: "1:5pm",
+    //   name: "steeven",
+    // },
+  ]);
+
+  const sendMsg = () => {
+    setMessages((prev) => [
+      ...prev,
+      {
+        message: "hai you have been disQualified",
+        time: "1:5pm",
+        name: "jojo",
+      },
+    ]);
+  };
   return (
     <div className="p-o h-screen w-full bg-gray-700 capitalize text-white md:p-10 lg:p-20">
       <div className="container flex h-screen w-full flex-col border-2 border-gray-800 md:h-auto md:rounded-lg">
@@ -20,7 +61,7 @@ const Chat = () => {
         <div className="midbar flex h-full w-full rounded-lg md:h-80 xl:h-96 ">
           <div className="leftsidebar hidden w-4/12 rounded-lg md:block">
             <div className="roomContainer p-5">
-              <h1 className="flex items-center justify-center space-x-4 rounded-md bg-gray-800 p-1">
+              <h1 className="flex items-center justify-center space-x-4 rounded-md bg-gray-800 p-2">
                 <span className="mr-1">
                   <MdOutlineMeetingRoom size="1.2em" />
                 </span>
@@ -37,12 +78,31 @@ const Chat = () => {
               </div>
             </div>
           </div>
-          <div className="msgSection no-scrollbar h-full w-full overflow-scroll  overscroll-none rounded-lg bg-white p-5 text-black text-gray-600 md:h-auto">
-            <p>something</p>
+          <div className="msgSection no-scrollbar h-full w-full space-y-5 overflow-scroll  overscroll-none rounded-lg bg-white p-5 text-left text-white md:h-auto">
+            {messages.map((message) => {
+              return (
+                <div
+                  className="message space-y-2 rounded-md bg-gray-800 p-2 text-xs text-gray-300"
+                  key={message.name}
+                >
+                  <div className="flex w-full items-center justify-between space-x-1">
+                    <p className="mr-1 text-sm text-white">{message.name}</p>
+                    <h1 className="text-xs text-gray-300">
+                      Today at {message.time}
+                    </h1>
+                  </div>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Aperiam, totam consequuntur delectus tempora molestias
+                    pariatur vel rem in ipsam libero.
+                  </p>
+                </div>
+              );
+            })}
           </div>
           <div className="rightsidebar hidden w-4/12 rounded-lg md:block">
             <div className="userContainer p-5">
-              <h1 className="flex items-center justify-center space-x-2 rounded-md bg-gray-800 p-1  text-center">
+              <h1 className="flex items-center justify-center space-x-2 rounded-md bg-gray-800 p-2  text-center">
                 <FaUsers size="1.2em" />
                 <span></span> users -- 5
               </h1>
@@ -64,7 +124,9 @@ const Chat = () => {
             className="w-full rounded-md py-2 px-2 text-black outline-none"
             placeholder="Enter Message"
           />
-          <button className="button bg-gray-700">Send</button>
+          <button className="button bg-gray-700" onClick={sendMsg}>
+            Send
+          </button>
         </div>
       </div>
     </div>
